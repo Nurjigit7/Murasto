170f39a39629c5e8c989b8889e96acab9d40a7f1
626 <nurjigitumarov22@gmail.com>
2023-07-14 20:59:47 +0300

    Initial commit

---
+++ src/Animal.java
@@ -0,0 +1,47 @@
+public class Animal {
+    private String name;
+    private int weight;
+    private String color;
+
+    public Animal(String name, int weight, String color) {
+        this.name = name;
+        this.weight = weight;
+        this.color = color;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public Animal() {
+    }
+
+    @Override
+    public String toString() {
+        return "Animal{" +
+                "name='" + name + '\'' +
+                ", weight=" + weight +
+                ", color='" + color + '\'' +
+                '}';
+    }
+}
